85f15e366d105825efe7b21a070edc350f626f6f
Rouf Ditta <shahreen.ahmed786@hotmail.com>
2024-02-21 00:04:33 +0000

    search input and search history js

--- starter/script.js
+++ starter/script.js
@@ -2,15 +2,15 @@ var cityInput = document.querySelector(".city-input")
 var weather = document.querySelector(".weather-input");
 var searchButton = document.querySelector("#search-button");
 var currentWeatherDiv = document.querySelector(".current-weather");
-var weatherCardsDiv = document.querySelector("#forecast");
+var forecastContainer = document.querySelector("#forecast");
 searchButton.addEventListener("click", getCityCoordinates);
 // API key for openweathermap
-var API_KEY = "f2eb18881281555f09869f2032b44d6c"; 
+var API_KEY = "f2eb18881281555f09869f2032b44d6c";
 
 // Function to create the weather card
 function createWeatherCard(cityName, weatherItem, index) {
     console.log(weatherItem, "this is the weather item")
-    if(index === 0) {
+    if (index === 0) {
         return `<div class="details">
                     <h3>${cityName} (${weatherItem.dt_txt.split("")[0]})</h3>
                     <h4>Temprature:${(weatherItem.main.temprature - 273.15).toFixed(2)}°C</h4>
@@ -21,8 +21,8 @@ function createWeatherCard(cityName, weatherItem, index) {
                     <img src="http://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
                     <h4>${weatherItem.weather[0].description}</h4>
                 </div>`;
-  
-//HTML for the 5 day forcast card
+
+        //HTML for the 5 day forcast card
     } else {
 
         return `<div class="days-forecast"> 
@@ -31,63 +31,63 @@ function createWeatherCard(cityName, weatherItem, index) {
                     <h4>Temprature:${(weatherItem.main.temprature - 273.15).toFixed(2)}°C</h4>
                     <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
                     <h4>Humidity: ${weatherItem.main.humidity}%</h4>
-                </div>`;     
-}}
+                </div>`;
+    }
+}
 
 // Function to fetch weather data
 function getWeatherDetails(cityName, lat, lon) {
-    var WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`; 
+    var WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-console.log(data, "this is get weather details")
+        console.log(data, "this is get weather details")
 
-        var fiveDayForcast = [data.list[0],data.list[8],data.list[16],data.list[24], data.list[32],data.list[39]]
-       
-//adding weather cards to the DOM
-for (index = 0; index < fiveDayForcast.length; index++) {
-    weatherItem = fiveDayForcast[index];
-    if ((index === 0)) {
-      currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index)
-      );
-    } else {
-      weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index)
-      );
-    }
-  }
+        var fiveDayForcast = [data.list[0], data.list[8], data.list[16], data.list[24], data.list[32], data.list[39]]
+
+        //adding weather cards to the DOM
+        for (index = 0; index < fiveDayForcast.length; index++) {
+            weatherItem = fiveDayForcast[index];
+            if ((index === 0)) {
+                currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index)
+                );
+            } else {
+                forecastContainer.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index)
+                );
+            }
+        }
     }).catch(() => {
         alert("An error occurred while fetching weather data.");
+       
     });
+}
+
+//retreave weather information form the API's
+function getWeather(cityName) {
+var GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+    console.log(data, "this is get city coordinates")
+    if (!data.length) return alert(`ERROR! No information found for ${cityName}`);
+    var { name, lat, lon } = data[0];
+    getWeatherDetails(name, lat, lon);
+    console.log(data)
+}).catch(() => {
+    alert("ERROR!");
+    cityName("London");
+});
+console.log(cityName)
 }
 
 // Function to handle search input
-function getCityCoordinates(e){
+function getCityCoordinates(e) {
     e.preventDefault();
-    // var cityName = $(".form-input").val();
-    // searchedCities.push(city);
-    // localStorage.setItem("cities", JSON.stringify(searchedCities));
-    // $("#searchHistory").append(`<div class="selected">${city}</div>`);
-    // $(".form-input").val("");
-    // getWeather(city);
+    var city= $(".form-input").val();
+    searchedCities.push(city);
+    localStorage.setItem("cities", JSON.stringify(searchedCities));
+    $("#searchHistory").append(`<div class="selected">${city}</div>`);
+    $(".form-input").val("");
+    getWeather(city);
 }
-
-   
-    var cityName = "london"
-   
-    var GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
-    
-
-//retreave weather information form the API's
-    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-        console.log(data, "this is get city coordinates")
-        if(!data.length) return alert(`ERROR! No information found for ${cityName}`);
-        var { name, lat, lon } = data[0];
-        getWeatherDetails(name, lat, lon);
-        console.log(data)
-    }).catch(() => {
-        alert("ERROR!");
-    });
-    console.log(cityName)
-
 
 // Function to display search history
 var searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
@@ -96,10 +96,11 @@ function searchHistory() {
     searchedCities.forEach(city => {
         $("#searchHistory").append(`<div class="selected">${city}</div>`);
     });
-// Call searchHistory function when the dashboard page loads
-searchHistory();
-console.log(city, "this is search history")
+    
+    // Call searchHistory function when the dashboard page loads
+    searchHistory();
+    console.log(city, "this is search history")
 }
-  
+
 
 
